9b20ff0fb5b4700ce32a91fa5831303e322f30ff
edersonlrf <edersonluis.rf@gmail.com>
2018-05-30 16:10:53 -0300

    hooks do codeigniter

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -11,3 +11,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
 */
+
+$hook['post_controller_constructor'] = array(
+    'class' => 'AuthHook',
+    'function' => 'check',
+    'filename' => 'AuthHook.php',
+    'filepath'  => 'hooks'
+);
---
+++ application/hooks/AuthHook.php
@@ -0,0 +1,25 @@
+<?php
+
+class AuthHook
+{
+    private $controller = [
+        'auth'
+    ];
+
+    public function check()
+    {
+        $CI =&  get_instance();
+
+        if (!isset($CI->session)) {
+            $CI->load->library('session');
+        };
+        $CI->load->helper('url');
+
+        $user = $CI->session->user ?? null;
+        $controller = $CI->uri->segment(1);
+        if (!$user and !in_array($controller, $this->controller)) {
+            redirect('auth/login', 'location', 302);
+            die();
+        }
+    }
+}
